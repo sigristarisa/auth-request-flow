279f1eff1d568ac3162770a0fc4c6c340864dbf7
sigristarisa <arisasigrist.ch@gmail.com>
2022-06-14 17:23:24 +0200

    finished credentials

--- src/controllers/token.js
+++ src/controllers/token.js
@@ -1,24 +1,12 @@
 const jwt = require("jsonwebtoken");
 
-const mockUser = {
-  username: "authguy",
-  password: "mypassword",
-  profile: {
-    firstName: "Chris",
-    lastName: "Wolstenholme",
-    age: 43,
-  },
-};
-
 const createToken = (req, res) => {
-  const { username, password, profile } = mockUser;
+  const { username, password, profile } = req.body;
   const payload = {};
   payload["username"] = username;
   payload["profile"] = profile;
 
-  console.log("extract password: ", password);
-
-  const token = jwt.sign(payload, "mypassword");
+  const token = jwt.sign(payload, password);
   res.json({ token });
 };
 
