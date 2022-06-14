9d16a470459ab162cd62b75621e2b6ab040160f0
sigristarisa <arisasigrist.ch@gmail.com>
2022-06-14 16:19:56 +0200

    created controller dir and extracted function

--- src/router.js
+++ src/router.js
@@ -1,30 +1,9 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
-
 const router = express.Router();
 
-// const mockUser = {
-//   username: "authguy",
-//   password: "mypassword",
-//   profile: {
-//     firstName: "Chris",
-//     lastName: "Wolstenholme",
-//     age: 43,
-//   },
-// };
+const { createToken } = require("./controllers/login");
 
-router.post("/login", (req, res) => {
-  const { username, password, profile } = req.body;
-  const payload = {};
-  payload["username"] = username;
-  payload["profile"] = profile;
-
-  const token = jwt.sign(payload, password);
-
-  console.log("this is the token", token);
-
-  res.json({ token });
-});
+router.post("/login", createToken);
 
 router.get("/profile", (req, res) => {});
 
