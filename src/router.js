9f1cc5634419a5ecc5c6910e4b7e8de2f9e417d5
sigristarisa <arisasigrist.ch@gmail.com>
2022-06-14 16:14:57 +0200

    finished Login

--- src/router.js
+++ src/router.js
@@ -1,25 +1,31 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
-const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
-};
+// const mockUser = {
+//   username: "authguy",
+//   password: "mypassword",
+//   profile: {
+//     firstName: "Chris",
+//     lastName: "Wolstenholme",
+//     age: 43,
+//   },
+// };
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
+  const { username, password, profile } = req.body;
+  const payload = {};
+  payload["username"] = username;
+  payload["profile"] = profile;
 
+  const token = jwt.sign(payload, password);
+
+  console.log("this is the token", token);
+
+  res.json({ token });
 });
 
-router.get('/profile', (req, res) => {
-  
-});
-
+router.get("/profile", (req, res) => {});
 
 module.exports = router;
